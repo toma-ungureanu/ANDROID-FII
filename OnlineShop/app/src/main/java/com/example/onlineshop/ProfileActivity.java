cf600f0b69a22da75bb0993d4b1afbdfaae639b6
toma-ungureanu <toma.ungureanu@info.uaic.ro>
2020-03-23 12:52:14 +0200

    Lab 5 Done

--- OnlineShop/app/src/main/java/com/example/onlineshop/ProfileActivity.java
+++ OnlineShop/app/src/main/java/com/example/onlineshop/ProfileActivity.java
@@ -4,12 +4,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.*;
+
 public class ProfileActivity extends AppCompatActivity
 {
+    private static final String FILE_NAME = "example.txt";
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,19 +24,99 @@ public class ProfileActivity extends AppCompatActivity
         setContentView(R.layout.activity_profile);
 
         final Button button = findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
                 onGoBackSelected();
             }
         });
 
         final Button button1 = findViewById(R.id.button_id1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
                 onChangePassword();
             }
         });
+
+        mEditText = findViewById(R.id.editTextDialogUserInput1);
     }
+
+    public void save(View v)
+    {
+        String text = mEditText.getText().toString();
+        FileOutputStream fos = null;
+
+        try
+        {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+            mEditText.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (fos != null)
+            {
+                try
+                {
+                    fos.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(View v)
+    {
+        FileInputStream fis = null;
+        try
+        {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null)
+            {
+                sb.append(text).append("\n");
+            }
+
+            mEditText.setText(sb.toString().replace("\n"," "));
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (fis != null)
+            {
+                try
+                {
+                    fis.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     public void onGoBackSelected()
     {
--- OnlineShop/app/src/main/java/com/example/onlineshop/SettingsActivity.java
+++ OnlineShop/app/src/main/java/com/example/onlineshop/SettingsActivity.java
@@ -1,13 +1,21 @@
 package com.example.onlineshop;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity
 {
+    private Button saveButton;
+    private Switch switch1;
+    public static final String SWITCH1 = "switch1";
+    private boolean switchOnOff;
+    public static final String SHARED_PREFS = "sharedPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,13 +23,40 @@ public class SettingsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        saveButton = findViewById(R.id.save_button);
+        switch1 = findViewById(R.id.switch1);
         final Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onGoBackSelected();
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
+        loadData();
+        updateViews();
     }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(SWITCH1, switch1.isChecked());
+        editor.apply();
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        switchOnOff = sharedPreferences.getBoolean(SWITCH1, false);
+    }
+
+    public void updateViews() { switch1.setChecked(switchOnOff); }
 
     public void onGoBackSelected()
     {
